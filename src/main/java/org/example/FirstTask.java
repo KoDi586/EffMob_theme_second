a43f19184ed465db2e91b7a2d2ac2f1bdc960f6b
DanKoDi <hirofool502@gmail.com>
2025-10-30 20:25:02 +0300

    first task (the two threads count in turn) finished

---
+++ src/main/java/org/example/FirstTask.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class FirstTask {
+
+    public void execute(Integer integer) {
+
+        ForSynchronized fs = new ForSynchronized();
+
+        Runnable first_runnable = () -> {
+            System.out.println("First thread is started");
+            for (int i = 0; i <= integer; i+=2) {
+                synchronized (fs) {
+                    fs.print(i);
+                    try {
+                        fs.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("Interrupted exception is in first_runnable");
+                    }
+                }
+            }
+            Thread.currentThread().interrupt();
+        };
+        Runnable second_runnable = () -> {
+            System.out.println("Second thread is started");
+            for (int i = 1; i <= integer; i+=2) {
+                synchronized (fs) {
+                    fs.print(i);
+                    try {
+                        fs.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("Interrupted exception in second_runnable");
+                    }
+                }
+            }
+            Thread.currentThread().interrupt();
+        };
+
+        Thread thread1 = new Thread(first_runnable);
+        Thread thread2 = new Thread(second_runnable);
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+}
+
+class ForSynchronized {
+    public synchronized void print(Integer integer) {
+
+        try {
+            System.out.println(integer);
+            notifyAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        FirstTask firstTask = new FirstTask();
+
+        firstTask.execute(10);
+
+    }
+}
